0f5b373b871c559ba6b66c935c93e65f410f02a7
Tihomir594 <valc0465@student.acitech.org>
2025-03-21 09:13:00 -0400

    Need to do functionality

---
+++ script.js
@@ -0,0 +1,7 @@
+const rock = document.getElementById("rock");
+let rock = 1;
+let paper = 2;
+if (player) = 3;
+if cpu chose 2 or cpu chose 4 "Player wins!"
+else if cpu chose chose 3 "Tie"
+else "Player loses."
