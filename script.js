b756c8aebb3e034ccf59c207525beed8fc5ee9ef
Tihomir594 <valc0465@student.acitech.org>
2025-03-25 09:10:25 -0400

    Have to do javascript

--- script.js
+++ script.js
@@ -3,10 +3,12 @@ const paper = document.getElementById("paper");
 const scissors = document.getElementById("scissors");
 const player = document.getElementById("human");
 const computer = document.getElementById("computer");
+const text = document.getElementById("text");
 rock.addEventListener("click",place_holder);
 clicks++;
 function place_holder() {
-    if ("player" = rock && "computer" = paper;)
+    if (player === "scissors" && computer === "paper");
+    text.innerHTML = "Scissors loses to paper!";
 }
 
 
